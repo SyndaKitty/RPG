bbe9a66d571e7c0988f726fae2a951bb59554d6b
Spencer Haney <theOfficialSkara@gmail.com>
2014-09-07 15:01:59 -0400

    Added basic Engine mainframe

---
+++ RPG/src/Game.java
@@ -0,0 +1,8 @@
+
+public abstract class Game {
+	
+	public abstract void getInput();
+	public abstract void update();
+	public abstract void render();
+	
+}
---
+++ RPG/src/Runescape.java
@@ -0,0 +1,19 @@
+
+public class Runescape extends Game{
+
+	@Override
+	public void getInput() {
+		
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+	@Override
+	public void render() {
+		
+	}
+
+}
